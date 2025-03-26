4f1d03d1731ff6eac9e58ff2634114675a77a668
Kim-Andre Kristiansen <kim-andre.kristiansen@katalon.com>
2025-03-26 14:55:08 +0700

    Finalized demo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,88 @@
-import React from 'react';
-import Counter from './Counter';
+import { styled, TextField } from "@mui/material";
+import { useFlags, useLDClient } from "launchdarkly-react-client-sdk";
+import React, { useEffect, useState } from "react";
+
+const AppContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginTop: "2rem",
+  height: "100vh",
+  gap: "1rem",
+});
+
+const FlagContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+});
+
+const InputsContainer = styled("div")({
+  display: "flex",
+  gap: "1rem",
+});
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <h1>React Vite App</h1>
-            <Counter />
-        </div>
-    );
+  const [settings, setSettings] = useState<{
+    organization: number;
+    account: number;
+    project: number;
+  }>({
+    organization: 1,
+    account: 2,
+    project: 3,
+  });
+  const flags = useFlags();
+  const ldClient = useLDClient();
+
+  useEffect(() => {
+    console.log("Identifying user with custom attributes", settings);
+    ldClient?.identify({
+      kind: "user",
+      key: "context-key-changing",
+      ...settings,
+    });
+  }, [settings]);
+
+  return (
+    <AppContainer>
+      <h1>Launch Darkly Custom Attributes test</h1>
+      <InputsContainer>
+        <TextField
+          label="Organization"
+          type="number"
+          value={settings.organization}
+          onChange={(e) =>
+            setSettings({ ...settings, organization: parseInt(e.target.value) })
+          }
+        />
+        <TextField
+          label="Account"
+          type="number"
+          value={settings.account}
+          onChange={(e) =>
+            setSettings({ ...settings, account: parseInt(e.target.value) })
+          }
+        />
+        <TextField
+          label="Project"
+          type="number"
+          value={settings.project}
+          onChange={(e) =>
+            setSettings({ ...settings, project: parseInt(e.target.value) })
+          }
+        />
+      </InputsContainer>
+      <FlagContainer>
+        <h2>Flags</h2>
+        {Object.entries(flags).map(([key, value]) => (
+          <div key={key}>
+            <strong>{key}</strong>: {value.toString()}
+          </div>
+        ))}
+      </FlagContainer>
+    </AppContainer>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./index.css";
+import { withLDProvider } from "launchdarkly-react-client-sdk";
+import { render } from "react-dom";
+// import { LDProvider } from "launchdarkly-react-client-sdk";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const LDProvider = withLDProvider({
+  clientSideID: "67e360b82219ab0989a1367a",
+  context: {
+    kind: "user",
+    key: "context-key-changing",
+    organization: 1,
+    account: 2,
+    project: 3,
+  }
+})(App);
+
+const rootElement = document.getElementById("root");
+render(<LDProvider />, rootElement);
+
